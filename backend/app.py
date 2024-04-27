062550662e3f8fc9cff7cb2939f7212578325854
Jiaojiao Song <sjj941229@gmail.com>
2024-04-26 20:55:29 -0400

    fix cors

--- backend/app.py
+++ backend/app.py
@@ -10,7 +10,7 @@ load_dotenv()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.environ.get('LOCAL_FRONTEND'), os.environ.get('CLIENT_DOMAIN')],
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["Access-Control-Allow-Origin"],
